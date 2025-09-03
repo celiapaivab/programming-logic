964f3d989cfb8764ef147d1d8decae2f260475be
celiapaivab <celiadepaivabruno@gmail.com>
2025-09-02 22:22:39 -0300

    Testes adicionados

--- src/calculadora.js
+++ src/calculadora.js
@@ -7,17 +7,6 @@ function mediaDoisNumeros(valor1, valor2) {
   return soma / 2;
 };
 
-function realizarSomaTela() {
-  const input1 = document.getElementById("input1");
-  const input2 = document.getElementById("input2");
-
-  if (input1 == 0 && input2 == 0) {
-    alert("Os valores não podem ser zero");
-  };
-
-  return somarDoisNumeros(Number(input1.value), Number(input2.value));
-};
-
 module.exports = {
   somarDoisNumeros
 };
--- tests/calculadora.test.js
+++
@@ -1,5 +0,0 @@
-const {somarDoisNumeros} = require('../src/calculadora')
-
-const resultadoSoma = somarDoisNumeros(5, 3);
-
-console.log(resultadoSoma);
